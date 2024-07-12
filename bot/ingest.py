5b3f7f806f7aec33bb64212f6626fd7f45d934f6
Ash2809 <aashutoshkumar2809@gmail.com>
2024-07-12 20:46:45 +0530

    commit

--- bot/ingest.py
+++ bot/ingest.py
@@ -1,4 +1,39 @@
+from langchain_astradb import AstraDBVectorStore
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from dotenv import load_dotenv
 import os
 import pandas as pd
+from bot.data_converter import data_converter
+
+load_dotenv()
+
+GOOGLE_API_KEY=os.getenv("OPENAI_API_KEY")
+API_ENDPOINT=os.getenv("API_ENDPOINT")
+ASTRA_API=os.getenv("ASTRA_API")
+ASTRA_KEYSPACE=os.getenv("ASTRA_KEYSPACE")
+
+gemini_embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+
+
+def ingest_data(status):
+    vector_store = AstraDBVectorStore(embedding = gemini_embeddings,
+                                      collection_name = "Ecom_Bot",
+                                      api_endpoint = API_ENDPOINT,
+                                      token = ASTRA_API,
+                                      namespace = ASTRA_KEYSPACE)
+    storage = status
+    
+    if storage == None:
+        docs = data_converter()
+        inserted_ids = vector_store.add_documents(docs)
+    else:
+        return vector_store
+    return vector_store,inserted_ids
+
+if __name__ == "__main__":
+    vector_store,inserted_ids = ingest_data(None)
+    print(f"inserted {len(inserted_ids)} documents.")
+    results = vector_store.similarity_search("can you tell me the low budget sound basshead.")
+    for res in results:
+            print(f"* {res.page_content} [{res.metadata}]")
+
---
+++ bot/retrieval_gen.py
@@ -0,0 +1,38 @@
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_google_genai import ChatGoogleGenerativeAI
+from bot.ingest import ingest_data
+
+def generation(vector_store):
+    retriever = vector_store.as_retriever(search_kwargs={"k": 3})
+
+    PRODUCT_BOT_TEMPLATE = """
+    You are an ecommerce-bot who is an expert in product recommendations and customer queries.
+    It analyzes product titles and reviews to provide accurate and helpful responses.
+    Ensure your answers are relevant to the product context and refrain from straying off-topic.
+    Your responses should be concise and informative.
+
+    CONTEXT:
+    {context}
+
+    QUESTION: {question}
+
+    YOUR ANSWER:
+    
+    """
+
+    prompt = ChatPromptTemplate.from_template(PRODUCT_BOT_TEMPLATE)
+
+    llm = ChatGoogleGenerativeAI(model="gemini-pro",
+                 temperature=0.6, top_p=0.8)
+    
+    chain = ({"context":retriever,"question":RunnablePassthrough()}|prompt|llm|StrOutputParser())
+
+    return chain
+
+if __name__ == "__main__":
+    vector_store = ingest_data("done")#parameter is done cuz ingest_data ran and DB has been created
+    chain  = generation(vector_store)
+    print(chain.invoke("can you tell me the best bluetooth buds?"))
+
